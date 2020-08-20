94f33d82799b25a06be862eb5abec0311256233d
João Vitor Dias <jvitordiass@outlook.com>
2020-08-19 22:18:12 -0300

    compound-interest

---
+++ modulo-3/compound-interest/src/pages/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './style.css'
+
+const Footer = () => (
+  <footer>
+    <p>Criador por <a href="https://github.com/diasjoaovitor" target="blank">João Vitor</a></p>
+  </footer>
+)
+
+export default Footer
